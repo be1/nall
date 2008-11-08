de3c088517dda536300027fc2329729e1c345d97
Benoît Rouits <brouits@free.fr>
2008-11-08 04:32:46 +0100

    added full copyright info

--- na.h
+++ na.h
@@ -1,7 +1,31 @@
 /* 
- * na.h Copyright © 2008 by Benoît Rouits
- * Published under the terms of the GNU General Public License (GPL).
+ * na.h Copyright © 2008 by Benoît Rouits <brouits@free.fr>
+ * Published under the terms of the GNU General Public License v2 (GPLv2).
+ * 
+ ***************************************************** 
+ * nall: a non-intrusive desktop all-purpose notifer *
+ *****************************************************
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301, USA.
+ * 
+ * see the COPYING file included in the csv2latex package or
+ * http://www.gnu.org/licenses/gpl.txt for the full licence
+ * 
  */
+
 #ifndef _NA_H
 #define _NA_H
 #include <gtk/gtk.h>
